a2053060848a16a2f19d1c3da26f00cc7f33dbc6
Oleg Kozlov <h1karo@relmsg.ru>
2021-05-22 00:21:59 +0300

    Implement property interface

---
+++ src/Token/PropertyInterface.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * This file is a part of Relations Messenger Json Web Token Implementation.
+ * This package is a part of Relations Messenger.
+ *
+ * @link      https://github.com/relmsg/json-web-token
+ * @link      https://dev.relmsg.ru/packages/json-web-token
+ * @copyright Copyright (c) 2018-2021 Relations Messenger
+ * @author    Oleg Kozlov <h1karo@relmsg.ru>
+ * @license   Apache License 2.0
+ * @license   https://legal.relmsg.ru/licenses/json-web-token
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RM\Standard\Jwt\Token;
+
+/**
+ * Interface PropertyInterface
+ *
+ * @author Oleg Kozlov <h1karo@relmsg.ru>
+ */
+interface PropertyInterface
+{
+    /**
+     * @return string
+     */
+    public function getName(): string;
+
+    /**
+     * @return mixed
+     */
+    public function getValue();
+
+    /**
+     * @param mixed
+     */
+    public function setValue($value);
+}
