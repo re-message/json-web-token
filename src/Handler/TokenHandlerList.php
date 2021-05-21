c460e1281ee5d473d92194151e7703a5154740d0
Oleg Kozlov <h1karo@relmsg.ru>
2021-05-21 22:56:07 +0300

    Update copyright

--- src/Handler/TokenHandlerList.php
+++ src/Handler/TokenHandlerList.php
@@ -5,7 +5,7 @@
  *
  * @link      https://github.com/relmsg/json-web-token
  * @link      https://dev.relmsg.ru/packages/json-web-token
- * @copyright Copyright (c) 2018-2020 Relations Messenger
+ * @copyright Copyright (c) 2018-2021 Relations Messenger
  * @author    Oleg Kozlov <h1karo@relmsg.ru>
  * @license   Apache License 2.0
  * @license   https://legal.relmsg.ru/licenses/json-web-token
