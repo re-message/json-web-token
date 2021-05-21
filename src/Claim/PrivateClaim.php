bf700fd33cc368229d225181f48acac6b4601f31
Oleg Kozlov <h1karo@relmsg.ru>
2021-05-22 02:28:04 +0300

    Implement public and private claims

---
+++ src/Claim/PrivateClaim.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * This file is a part of Relations Messenger Json Web Token Implementation.
+ * This package is a part of Relations Messenger.
+ *
+ * @link      https://github.com/relmsg/json-web-token
+ * @link      https://dev.relmsg.ru/packages/json-web-token
+ * @copyright Copyright (c) 2018-2021 Relations Messenger
+ * @author    Oleg Kozlov <h1karo@relmsg.ru>
+ * @license   Apache License 2.0
+ * @license   https://legal.relmsg.ru/licenses/json-web-token
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RM\Standard\Jwt\Claim;
+
+use RM\Standard\Jwt\Token\AbstractProperty;
+
+/**
+ * Class PrivateClaim
+ *
+ * @author Oleg Kozlov <h1karo@relmsg.ru>
+ */
+class PrivateClaim extends AbstractProperty implements ClaimInterface
+{
+    private string $name;
+
+    public function __construct(string $name, $value = null)
+    {
+        parent::__construct($value);
+
+        $this->setName($name);
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
