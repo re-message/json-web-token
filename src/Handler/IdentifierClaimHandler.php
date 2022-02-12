9d065f2c5a399d0608bc9806599a3d51050d3c43
Oleg Kozlov <h1karo@relmsg.ru>
2022-02-13 00:42:27 +0300

    Make identifier claim handler a delegating

--- src/Handler/IdentifierClaimHandler.php
+++ src/Handler/IdentifierClaimHandler.php
@@ -16,72 +16,27 @@
 
 namespace RM\Standard\Jwt\Handler;
 
-use RM\Standard\Jwt\Exception\IncorrectPropertyTypeException;
+use RM\Standard\Jwt\Generator\IdentifierGenerator;
 use RM\Standard\Jwt\Identifier\IdentifierGeneratorInterface;
-use RM\Standard\Jwt\Identifier\UniqIdGenerator;
 use RM\Standard\Jwt\Property\Payload\Identifier;
-use RM\Standard\Jwt\Storage\RuntimeTokenStorage;
 use RM\Standard\Jwt\Storage\TokenStorageInterface;
-use RM\Standard\Jwt\Token\PropertyInterface;
-use RM\Standard\Jwt\Token\PropertyTarget;
-use UnexpectedValueException;
+use RM\Standard\Jwt\Validator\Property\IdentifierValidator;
 
 /**
  * Class IdentifierClaimHandler provides processing for { @see Identifier } claim.
  *
  * @author Oleg Kozlov <h1karo@relmsg.ru>
  */
-class IdentifierClaimHandler extends AbstractPropertyHandler
+class IdentifierClaimHandler extends DelegatingPropertyHandler
 {
-    protected IdentifierGeneratorInterface $identifierGenerator;
-    protected TokenStorageInterface $tokenStorage;
-
-    /**
-     * Duration of token in seconds. By default, 1 hour.
-     * For security reason, cannot be infinite.
-     */
-    protected int $duration = 60 * 60;
-
     public function __construct(
-        IdentifierGeneratorInterface $generator = null,
+        IdentifierGeneratorInterface $identifierGenerator = null,
         TokenStorageInterface $storage = null,
         int $duration = 60 * 60
     ) {
-        $this->identifierGenerator = $generator ?? new UniqIdGenerator();
-        $this->tokenStorage = $storage ?? new RuntimeTokenStorage();
-        $this->duration = $duration;
-    }
+        $generator = new IdentifierGenerator($identifierGenerator, $storage, $duration);
+        $validator = new IdentifierValidator($storage);
 
-    public function getPropertyTarget(): PropertyTarget
-    {
-        return PropertyTarget::PAYLOAD;
-    }
-
-    public function getPropertyName(): string
-    {
-        return Identifier::NAME;
-    }
-
-    protected function generateProperty(): Identifier
-    {
-        $identifier = $this->identifierGenerator->generate();
-        $this->tokenStorage->put($identifier, $this->duration);
-
-        return new Identifier($identifier);
-    }
-
-    protected function validateProperty(PropertyInterface $property): bool
-    {
-        if (!$property instanceof Identifier) {
-            $message = sprintf('%s can handle only %s.', static::class, $property::class);
-            throw new UnexpectedValueException($message);
-        }
-
-        $value = $property->getValue();
-        if (!is_string($value)) {
-            throw new IncorrectPropertyTypeException('string', gettype($value), $this->getPropertyName());
-        }
-
-        return $this->tokenStorage->has($value);
+        parent::__construct($generator, $validator);
     }
 }
