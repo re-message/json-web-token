aff98fbcfdcfe4c9a3b6b4057c15ef0e00bfbea7
Oleg Kozlov <h1karo@relmsg.ru>
2022-04-08 23:23:49 +0300

    Implement sign test

--- tests/Signature/SignerTest.php
+++ tests/Signature/SignerTest.php
@@ -16,10 +16,15 @@
 
 namespace RM\Standard\Jwt\Tests\Signature;
 
+use BenTools\CartesianProduct\CartesianProduct;
+use Generator;
 use Laminas\Math\Rand;
 use ParagonIE\ConstantTime\Base64UrlSafe;
 use PHPUnit\Framework\TestCase;
+use RM\Standard\Jwt\Algorithm\AlgorithmInterface;
+use RM\Standard\Jwt\Algorithm\Signature\HMAC\HS256;
 use RM\Standard\Jwt\Algorithm\Signature\HMAC\HS3256;
+use RM\Standard\Jwt\Algorithm\Signature\SignatureAlgorithmInterface;
 use RM\Standard\Jwt\Exception\InvalidTokenException;
 use RM\Standard\Jwt\Key\KeyInterface;
 use RM\Standard\Jwt\Key\OctetKey;
@@ -28,10 +33,25 @@ use RM\Standard\Jwt\Signature\Signer;
 
 class SignerTest extends TestCase
 {
+    /**
+     * @dataProvider provideKeyAndAlgorithm
+     */
+    public function testSign(SignatureAlgorithmInterface $algorithm, KeyInterface $key): void
+    {
+        $token = SignatureToken::createWithAlgorithm($algorithm);
+
+        $signer = new Signer();
+        $signed = $signer->sign($token, $algorithm, $key);
+
+        self::assertNotSame($signed, $token);
+        self::assertFalse($token->isSigned());
+        self::assertTrue($signed->isSigned());
+    }
+
     public function testSignAlreadySignedToken(): void
     {
         $algorithm = new HS3256();
-        $key = $this->generateKey();
+        $key = $this->generateOctetKey();
         $token = SignatureToken::createWithAlgorithm($algorithm);
 
         $signer = new Signer();
@@ -42,7 +62,37 @@ class SignerTest extends TestCase
         $signer->sign($signed, $algorithm, $key);
     }
 
-    public function generateKey(): KeyInterface
+    public function provideKeyAndAlgorithm(): Generator
+    {
+        $cartesian = new CartesianProduct(
+            [
+                iterator_to_array($this->getAlgorithms()),
+                iterator_to_array($this->getKeys()),
+            ]
+        );
+
+        /**
+         * @var AlgorithmInterface $algorithm
+         * @var KeyInterface $key
+         */
+        foreach ($cartesian->getIterator() as [$algorithm, $key]) {
+            $name = sprintf('alg %s and key %s', $algorithm->name(), $key->getType());
+            yield $name => [$algorithm, $key];
+        }
+    }
+
+    public function getAlgorithms(): Generator
+    {
+        yield new HS256();
+        yield new HS3256();
+    }
+
+    public function getKeys(): Generator
+    {
+        yield $this->generateOctetKey();
+    }
+
+    public function generateOctetKey(): KeyInterface
     {
         return new OctetKey(Base64UrlSafe::encode(Rand::getBytes(64)));
     }
