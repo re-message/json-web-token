fb4620ce181a771516ab8179066f356b3115ead2
Oleg Kozlov <h1karo@relmsg.ru>
2022-04-18 21:17:45 +0300

    Fix serializer test payload case
    After changes `typ` parameter always will be first

--- tests/Serializer/SignatureCompactSerializerTest.php
+++ tests/Serializer/SignatureCompactSerializerTest.php
@@ -74,7 +74,7 @@ class SignatureCompactSerializerTest extends TestCase
     {
         yield [
             true,
-            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
         ];
 
         yield [
