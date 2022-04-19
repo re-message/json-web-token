e953c6cb0bc09530e3b93675b457319833d1fdf5
Oleg Kozlov <h1karo@relmsg.ru>
2022-04-20 00:33:45 +0300

    Implement formatter contract with json implementation

---
+++ src/Serializer/Format/JsonFormatter.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * This file is a part of Relations Messenger Json Web Token Implementation.
+ * This package is a part of Relations Messenger.
+ *
+ * @link      https://github.com/relmsg/json-web-token
+ * @link      https://dev.relmsg.ru/packages/json-web-token
+ * @copyright Copyright (c) 2018-2022 Relations Messenger
+ * @author    Oleg Kozlov <h1karo@relmsg.ru>
+ * @license   Apache License 2.0
+ * @license   https://legal.relmsg.ru/licenses/json-web-token
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RM\Standard\Jwt\Serializer\Format;
+
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+
+/**
+ * @author Oleg Kozlov <h1karo@relmsg.ru>
+ */
+class JsonFormatter implements FormatterInterface
+{
+    private JsonEncoder $encoder;
+
+    public function __construct()
+    {
+        $this->encoder = new JsonEncoder();
+    }
+
+    public function encode(array $data): string
+    {
+        return $this->encoder->encode($data, 'json');
+    }
+
+    public function decode(string $data): array
+    {
+        return $this->encoder->decode($data, 'json');
+    }
+}
