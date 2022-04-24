355ff07e7505add548750b41501e2b91f71bf176
Oleg Kozlov <h1karo@remessage.ru>
2022-04-25 01:28:00 +0300

    Implement deserialize test

--- tests/Key/Set/KeySetSerializerTest.php
+++ tests/Key/Set/KeySetSerializerTest.php
@@ -19,8 +19,11 @@ namespace RM\Standard\Jwt\Tests\Key\Set;
 use PHPUnit\Framework\TestCase;
 use RM\Standard\Jwt\Format\FormatterInterface;
 use RM\Standard\Jwt\Key\Factory\KeyFactoryInterface;
+use RM\Standard\Jwt\Key\Factory\OctetKeyFactory;
+use RM\Standard\Jwt\Key\KeyInterface;
 use RM\Standard\Jwt\Key\OctetKey;
 use RM\Standard\Jwt\Key\Set\KeySetSerializer;
+use RM\Standard\Jwt\Key\Set\KeySetSerializerInterface;
 
 /**
  * @covers \RM\Standard\Jwt\Key\Set\KeySetSerializer
@@ -58,6 +61,33 @@ class KeySetSerializerTest extends TestCase
                 ],
             ],
         ];
+    }
+
+    /**
+     * @dataProvider provideKeys
+     */
+    public function testDeserialize(array $expected): void
+    {
+        $factory = new OctetKeyFactory();
+
+        $keyToArray = static fn (KeyInterface $key) => $key->all();
+        $keysArray = array_map($keyToArray, $expected);
+        $keySet = [KeySetSerializerInterface::PARAM_KEYS => $keysArray];
+
+        $formatter = $this->createMock(FormatterInterface::class);
+        $formatter->method('decode')->willReturn($keySet);
+
+        $serializer = new KeySetSerializer($factory, $formatter);
+
+        $keys = $serializer->deserialize('any');
+        self::assertEquals($expected, $keys);
+    }
+
+    public function provideKeys(): iterable
+    {
+        yield 'empty' => [[]];
+
+        yield 'one octet key' => [[$this->getOctetKey()]];
     }
 
     private function getOctetKey(): OctetKey
