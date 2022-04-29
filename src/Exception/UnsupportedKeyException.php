774aea1cfa065446fee9e5c14df82b8903891cb8
Oleg Kozlov <h1karo@remessage.ru>
2022-04-29 23:14:45 +0300

    Allow pass null type to unsupported key exception

--- src/Exception/UnsupportedKeyException.php
+++ src/Exception/UnsupportedKeyException.php
@@ -27,9 +27,10 @@ class UnsupportedKeyException extends InvalidKeyException
     /**
      * @param class-string<KeyFactoryInterface> $factory
      */
-    public function __construct(string $type, string $factory, ?Throwable $previous = null)
+    public function __construct(?string $type, string $factory, ?Throwable $previous = null)
     {
-        $message = sprintf('Key with type "%s" does not supported by %s', $type, $factory);
+        $type ??= 'unknown';
+        $message = sprintf('Key with type "%s" does not supported by %s.', $type, $factory);
 
         parent::__construct($message, $previous);
     }
