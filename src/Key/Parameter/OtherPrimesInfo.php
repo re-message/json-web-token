e906963fceeb6a93b24a04a20d34bb50a1df1f91
Oleg Kozlov <h1karo@remessage.ru>
2022-05-05 22:28:04 +0300

    Implement RSA key parameters

---
+++ src/Key/Parameter/OtherPrimesInfo.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * This file is a part of Re Message Json Web Token implementation.
+ * This package is a part of Re Message.
+ *
+ * @link      https://github.com/re-message/json-web-token
+ * @link      https://dev.remessage.ru/packages/json-web-token
+ * @copyright Copyright (c) 2018-2022 Re Message
+ * @author    Oleg Kozlov <h1karo@remessage.ru>
+ * @license   Apache License 2.0
+ * @license   https://legal.remessage.ru/licenses/json-web-token
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RM\Standard\Jwt\Key\Parameter;
+
+/**
+ * @template-extends KeyParameter<array>
+ *
+ * @author Oleg Kozlov <h1karo@remessage.ru>
+ */
+class OtherPrimesInfo extends KeyParameter
+{
+    public const NAME = 'oth';
+
+    public function __construct(array $value)
+    {
+        parent::__construct(self::NAME, $value);
+    }
+}
