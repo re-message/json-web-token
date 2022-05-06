9b1519ddb1c05dc398a4b4a6ef749fa9fd4bfa05
Oleg Kozlov <h1karo@remessage.ru>
2022-05-06 19:54:05 +0300

    Implement rsa phpseclib transformer

---
+++ src/Key/Transformer/SecLib/AbstractSecLibTransformer.php
@@ -0,0 +1,114 @@
+<?php
+/*
+ * This file is a part of Re Message Json Web Token implementation.
+ * This package is a part of Re Message.
+ *
+ * @link      https://github.com/re-message/json-web-token
+ * @link      https://dev.remessage.ru/packages/json-web-token
+ * @copyright Copyright (c) 2018-2022 Re Message
+ * @author    Oleg Kozlov <h1karo@remessage.ru>
+ * @license   Apache License 2.0
+ * @license   https://legal.remessage.ru/licenses/json-web-token
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RM\Standard\Jwt\Key\Transformer\SecLib;
+
+use InvalidArgumentException;
+use ParagonIE\ConstantTime\Base64UrlSafe;
+use phpseclib3\Crypt\Common\AsymmetricKey;
+use phpseclib3\Crypt\PublicKeyLoader;
+use phpseclib3\Math\BigInteger;
+use RM\Standard\Jwt\Exception\InvalidKeyException;
+use RM\Standard\Jwt\Key\Factory\KeyFactoryInterface;
+use RM\Standard\Jwt\Key\KeyInterface;
+
+/**
+ * @template T of AsymmetricKey
+ * @template-implements SecLibTransformerInterface<T>
+ *
+ * @author Oleg Kozlov <h1karo@remessage.ru>
+ */
+abstract class AbstractSecLibTransformer implements SecLibTransformerInterface
+{
+    public function __construct(
+        private readonly KeyFactoryInterface $factory,
+    ) {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function transform(KeyInterface $key, string $type): AsymmetricKey
+    {
+        if (!$this->supports($type)) {
+            $message = sprintf(
+                '%s does not support the key %s.',
+                static::class,
+                $type,
+            );
+
+            throw new InvalidArgumentException($message);
+        }
+
+        $components = $this->toComponents($key->all());
+        $target = PublicKeyLoader::load($components);
+        if (is_a($target, $type, false)) {
+            return $target;
+        }
+
+        $message = sprintf(
+            'Returned key must implement %s, given %s.',
+            $type,
+            $target::class
+        );
+
+        throw new InvalidKeyException($message);
+    }
+
+    abstract protected function toComponents(array $parameters): array;
+
+    protected function toComponent(string $value): BigInteger
+    {
+        $bytes = Base64UrlSafe::decode($value);
+
+        return new BigInteger($bytes, 256);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function reverseTransform(AsymmetricKey $key): KeyInterface
+    {
+        if (!$this->supports($key::class)) {
+            $message = sprintf(
+                '%s does not support the key %s.',
+                static::class,
+                $key::class,
+            );
+
+            throw new InvalidArgumentException($message);
+        }
+
+        /** @var array $components */
+        $components = $key->toString('raw');
+        $content = $this->fromComponents($components);
+
+        if (!$this->factory->supports($content)) {
+            throw new InvalidKeyException('Unable to create key from phpseclib.');
+        }
+
+        return $this->factory->create($content);
+    }
+
+    abstract protected function fromComponents(array $components): array;
+
+    protected function fromComponent(BigInteger $value): string
+    {
+        $bytes = $value->toBytes();
+
+        return Base64UrlSafe::encodeUnpadded($bytes);
+    }
+}
---
+++ src/Key/Transformer/SecLib/RsaSecLibTransformer.php
@@ -0,0 +1,100 @@
+<?php
+/*
+ * This file is a part of Re Message Json Web Token implementation.
+ * This package is a part of Re Message.
+ *
+ * @link      https://github.com/re-message/json-web-token
+ * @link      https://dev.remessage.ru/packages/json-web-token
+ * @copyright Copyright (c) 2018-2022 Re Message
+ * @author    Oleg Kozlov <h1karo@remessage.ru>
+ * @license   Apache License 2.0
+ * @license   https://legal.remessage.ru/licenses/json-web-token
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RM\Standard\Jwt\Key\Transformer\SecLib;
+
+use phpseclib3\Crypt\RSA;
+use RM\Standard\Jwt\Exception\InvalidKeyException;
+use RM\Standard\Jwt\Key\Factory\RsaKeyFactory;
+use RM\Standard\Jwt\Key\Parameter\FirstCoefficient;
+use RM\Standard\Jwt\Key\Parameter\FirstFactorExponent;
+use RM\Standard\Jwt\Key\Parameter\FirstPrimeFactor;
+use RM\Standard\Jwt\Key\Parameter\Modulus;
+use RM\Standard\Jwt\Key\Parameter\PrivateExponent;
+use RM\Standard\Jwt\Key\Parameter\PublicExponent;
+use RM\Standard\Jwt\Key\Parameter\SecondFactorExponent;
+use RM\Standard\Jwt\Key\Parameter\SecondPrimeFactor;
+use RM\Standard\Jwt\Key\Parameter\Type;
+
+/**
+ * @template T of RSA
+ * @template-extends AbstractSecLibTransformer<T>
+ *
+ * @author Oleg Kozlov <h1karo@remessage.ru>
+ *
+ * @see RSA\Formats\Keys\Raw::load
+ */
+class RsaSecLibTransformer extends AbstractSecLibTransformer
+{
+    protected function toComponents(array $parameters): array
+    {
+        $parameters = $this->filterRsaParameters($parameters);
+
+        $publicExponent = $parameters[PublicExponent::NAME] ?? null;
+        $modulus = $parameters[Modulus::NAME] ?? null;
+
+        if (null === $publicExponent || null === $modulus) {
+            throw new InvalidKeyException('Exponent and/or modulus not found.');
+        }
+
+        return array_map([$this, 'toComponent'], $parameters);
+    }
+
+    protected function fromComponents(array $components): array
+    {
+        $notNull = static fn (mixed $value) => null !== $value;
+
+        $flattedComponents = array_filter(
+            [
+                PublicExponent::NAME => $components['e'] ?? null,
+                Modulus::NAME => $components['n'] ?? null,
+                PrivateExponent::NAME => $components['d'] ?? null,
+                FirstPrimeFactor::NAME => $components['primes'][1] ?? null,
+                SecondPrimeFactor::NAME => $components['primes'][2] ?? null,
+                FirstFactorExponent::NAME => $components['exponents'][1] ?? null,
+                SecondFactorExponent::NAME => $components['exponents'][2] ?? null,
+                FirstCoefficient::NAME => $components['coefficients'][2] ?? null,
+            ],
+            $notNull,
+        );
+
+        $parameters = array_map([$this, 'fromComponent'], $flattedComponents);
+
+        $exponentExists = array_key_exists(PublicExponent::NAME, $parameters);
+        $modulusExists = array_key_exists(Modulus::NAME, $parameters);
+        if (!$exponentExists || !$modulusExists) {
+            throw new InvalidKeyException('Exponent and/or modulus not found.');
+        }
+
+        return array_merge([Type::NAME => Type::RSA], $parameters);
+    }
+
+    private function filterRsaParameters(array $parameters): array
+    {
+        $rsaParameters = array_keys(RsaKeyFactory::DEFAULT_CLASS_MAP);
+        $isRsaParameter = static fn (string $name) => in_array($name, $rsaParameters, true);
+
+        return array_filter($parameters, $isRsaParameter, ARRAY_FILTER_USE_KEY);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function supports(string $type): bool
+    {
+        return is_a($type, RSA::class, true);
+    }
+}
