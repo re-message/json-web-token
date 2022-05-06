e339b799afc4129cb5299c7ec894cb2bf691f529
Oleg Kozlov <h1karo@remessage.ru>
2022-05-07 01:30:22 +0300

    Implement octet key generator

---
+++ src/Key/Generator/OctetKeyGenerator.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ * This file is a part of Re Message Json Web Token implementation.
+ * This package is a part of Re Message.
+ *
+ * @link      https://github.com/re-message/json-web-token
+ * @link      https://dev.remessage.ru/packages/json-web-token
+ * @copyright Copyright (c) 2018-2022 Re Message
+ * @author    Oleg Kozlov <h1karo@remessage.ru>
+ * @license   Apache License 2.0
+ * @license   https://legal.remessage.ru/licenses/json-web-token
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RM\Standard\Jwt\Key\Generator;
+
+use InvalidArgumentException;
+use Laminas\Math\Rand;
+use ParagonIE\ConstantTime\Base64UrlSafe;
+use RM\Standard\Jwt\Key\Key;
+use RM\Standard\Jwt\Key\KeyInterface;
+use RM\Standard\Jwt\Key\KeyOperation;
+use RM\Standard\Jwt\Key\KeyUsage;
+use RM\Standard\Jwt\Key\Parameter\Identifier;
+use RM\Standard\Jwt\Key\Parameter\KeyUse;
+use RM\Standard\Jwt\Key\Parameter\Operations;
+use RM\Standard\Jwt\Key\Parameter\Type;
+use RM\Standard\Jwt\Key\Parameter\Value;
+use RM\Standard\Jwt\Key\Thumbprint\ThumbprintFactory;
+use RM\Standard\Jwt\Key\Thumbprint\ThumbprintFactoryInterface;
+
+/**
+ * @author Oleg Kozlov <h1karo@remessage.ru>
+ */
+class OctetKeyGenerator implements KeyGeneratorInterface
+{
+    public function __construct(
+        private readonly int $length = 64,
+        private readonly ThumbprintFactoryInterface $thumbprintFactory = new ThumbprintFactory(),
+    ) {
+    }
+
+    public function generate(string $type): KeyInterface
+    {
+        if (!$this->supports($type)) {
+            $message = sprintf(
+                '%s can not generate a key with type "%s".',
+                static::class,
+                $type,
+            );
+
+            throw new InvalidArgumentException($message);
+        }
+
+        $bytes = Rand::getBytes($this->length);
+        $value = Base64UrlSafe::encodeUnpadded($bytes);
+
+        $key = new Key(
+            [
+                new Type(Type::OCTET),
+                new Value($value),
+                KeyUse::fromEnum(KeyUsage::SIGN),
+                Operations::fromEnum([KeyOperation::SIGN, KeyOperation::VERIFY]),
+            ]
+        );
+
+        $thumbprint = $this->thumbprintFactory->create($key);
+        $key->set(new Identifier($thumbprint));
+
+        return $key;
+    }
+
+    public function supports(string $type): bool
+    {
+        return Type::OCTET === $type;
+    }
+}
