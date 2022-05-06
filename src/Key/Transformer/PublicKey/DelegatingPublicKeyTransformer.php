36ff20806fd81c8a719aed2aff3d927c06dfa6a8
Oleg Kozlov <h1karo@remessage.ru>
2022-05-06 17:28:13 +0300

    Implement public key transformers

---
+++ src/Key/Transformer/PublicKey/DelegatingPublicKeyTransformer.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ * This file is a part of Re Message Json Web Token implementation.
+ * This package is a part of Re Message.
+ *
+ * @link      https://github.com/re-message/json-web-token
+ * @link      https://dev.remessage.ru/packages/json-web-token
+ * @copyright Copyright (c) 2018-2022 Re Message
+ * @author    Oleg Kozlov <h1karo@remessage.ru>
+ * @license   Apache License 2.0
+ * @license   https://legal.remessage.ru/licenses/json-web-token
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RM\Standard\Jwt\Key\Transformer\PublicKey;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use InvalidArgumentException;
+use RM\Standard\Jwt\Key\KeyInterface;
+
+/**
+ * @author Oleg Kozlov <h1karo@remessage.ru>
+ */
+class DelegatingPublicKeyTransformer implements PublicKeyTransformerInterface
+{
+    /**
+     * @var Collection<int, PublicKeyTransformerInterface>
+     */
+    private readonly Collection $collection;
+
+    /**
+     * @param iterable<PublicKeyTransformerInterface> $transformers
+     */
+    public function __construct(iterable $transformers = [])
+    {
+        $this->collection = new ArrayCollection();
+
+        foreach ($transformers as $transformer) {
+            $this->pushTransformer($transformer);
+        }
+    }
+
+    public function pushTransformer(PublicKeyTransformerInterface $transformer): void
+    {
+        $this->collection->add($transformer);
+    }
+
+    public function transform(KeyInterface $privateKey): KeyInterface
+    {
+        $type = $privateKey->getType();
+        $transformer = $this->findTransformer($type);
+        if (null === $transformer) {
+            $message = sprintf('The key with type "%s" not supported.', $type);
+
+            throw new InvalidArgumentException($message);
+        }
+
+        return $transformer->transform($privateKey);
+    }
+
+    public function supports(string $type): bool
+    {
+        return null !== $this->findTransformer($type);
+    }
+
+    protected function findTransformer(string $type): ?PublicKeyTransformerInterface
+    {
+        foreach ($this->collection as $transformer) {
+            if ($transformer->supports($type)) {
+                return $transformer;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ src/Key/Transformer/PublicKey/RsaPublicKeyTransformer.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * This file is a part of Re Message Json Web Token implementation.
+ * This package is a part of Re Message.
+ *
+ * @link      https://github.com/re-message/json-web-token
+ * @link      https://dev.remessage.ru/packages/json-web-token
+ * @copyright Copyright (c) 2018-2022 Re Message
+ * @author    Oleg Kozlov <h1karo@remessage.ru>
+ * @license   Apache License 2.0
+ * @license   https://legal.remessage.ru/licenses/json-web-token
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RM\Standard\Jwt\Key\Transformer\PublicKey;
+
+use InvalidArgumentException;
+use RM\Standard\Jwt\Key\Key;
+use RM\Standard\Jwt\Key\KeyInterface;
+use RM\Standard\Jwt\Key\Parameter\FirstCoefficient;
+use RM\Standard\Jwt\Key\Parameter\FirstFactorExponent;
+use RM\Standard\Jwt\Key\Parameter\FirstPrimeFactor;
+use RM\Standard\Jwt\Key\Parameter\KeyParameterInterface;
+use RM\Standard\Jwt\Key\Parameter\PrivateExponent;
+use RM\Standard\Jwt\Key\Parameter\SecondFactorExponent;
+use RM\Standard\Jwt\Key\Parameter\SecondPrimeFactor;
+use RM\Standard\Jwt\Key\Parameter\Type;
+
+/**
+ * @author Oleg Kozlov <h1karo@remessage.ru>
+ */
+class RsaPublicKeyTransformer implements PublicKeyTransformerInterface
+{
+    public const PRIVATE_PARAMETERS = [
+        PrivateExponent::NAME,
+        FirstPrimeFactor::NAME,
+        SecondPrimeFactor::NAME,
+        FirstFactorExponent::NAME,
+        SecondFactorExponent::NAME,
+        FirstCoefficient::NAME,
+    ];
+
+    public function transform(KeyInterface $privateKey): KeyInterface
+    {
+        $type = $privateKey->getType();
+        if (!$this->supports($type)) {
+            $message = sprintf(
+                '%s does not support key with type "%s".',
+                static::class,
+                $type,
+            );
+
+            throw new InvalidArgumentException($message);
+        }
+
+        $parameters = array_filter($privateKey->getParameters(), [$this, 'isPublic']);
+
+        return new Key($parameters);
+    }
+
+    protected function isPublic(KeyParameterInterface $parameter): bool
+    {
+        $name = $parameter->getName();
+
+        return !in_array($name, self::PRIVATE_PARAMETERS, true);
+    }
+
+    public function supports(string $type): bool
+    {
+        return Type::RSA === $type;
+    }
+}
